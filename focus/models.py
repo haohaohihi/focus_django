ffa0e86822f361a831863a52c6bde93409fb72b4
haohao <haohaohihi@gmail.com>
2017-02-26 17:04:29 +0800

    create ArticleModel,urls,  configure mysql-connection and some settings

--- focus/models.py
+++ focus/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class ArticleModel(models.Model):
+	article_title = models.CharField(max_length=200)
+	article_desc = models.CharField(max_length=1000, blank=True)
+	article_link = models.URLField(null=True)
+	article_source = models.CharField(max_length=50)
+	article_date = models.IntegerField()
+	article_category = models.CharField(max_length=50, blank=True)
+	picture_link = models.URLField(null=True, blank=True)
+
+	class Meta:
+		db_table = 'article'
--- focus_django/settings.py
+++ focus_django/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'focus',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,11 @@ WSGI_APPLICATION = 'focus_django.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': 'localhost',
+        'NAME': 'focus',
+        'USER': 'focus',
+        'PASSWORD': '657481',
     }
 }
 
@@ -103,9 +107,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hans'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
