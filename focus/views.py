422133aea61d0fe5eec7397e0579fcab512207f3
haohao <haohaohihi@gmail.com>
2017-02-27 08:49:56 +0800

    add logging

--- focus/views.py
+++ focus/views.py
@@ -1,5 +1,10 @@
+import logging
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
+
+logger = logging.getLogger("focus")
+
 def index(request):
+	logger.info("test logging")
 	return HttpResponse("hello")
