ee9e2215eb52f0f0f2b7ceb638d6b4b0b8704516
haohao <haohaohihi@gmail.com>
2017-02-27 16:08:27 +0800

    re-arrange views and urls logic

--- focus/views.py
+++ focus/views.py
@@ -9,56 +9,40 @@ logger = logging.getLogger("focus")
 
 
 def index(request):
-	logger.info("test logging")
-	articles = ArticleModel.objects.filter(article_category="products")
-	paginator = Paginator(articles, 10)
-	page = 1
-	page_dict = {
-		'articles': paginator.page(1),
-		'pre_page': page,
-		'next_page': page + 1,
-		'page': page,
-		'pages': paginator.num_pages,
-	}
-	return render(request, 'focus/index.html', page_dict)
+    articles = ArticleModel.objects.filter(article_category="products").order_by('-article_date')
+    paginator = Paginator(articles, 10)
+    page = 1
+    page_dict = {
+        'articles': paginator.page(1),
+        'pre_page': page,
+        'next_page': page + 1,
+        'page': page,
+        'pages': paginator.num_pages,
+    }
+    return render(request, 'focus/index.html', page_dict)
 
 
-def products_page(request, page):
-	return index(request)
-
-
-def news_page(request, page):
-	return index(request)
-
-
-def design_page(request, page):
-	return index(request)
-
-
-def marketing_page(request, page):
-	return index(request)
-
-
-def dev(request):
-	return index(request)
-
-
-def dev_web_page(request, page):
-	return index(request)
-
-
-def dev_python_page(request, page):
-	return index(request)
-
-
-def dev_linux_page(request, page):
-	return index(request)
-
-
-def dev_db_page(request, page):
-	return index(request)
-
-
-def dev_android_page(request, page):
-	return index(request)
-
+def get_page(request, page=1, category="products", sub_category=""):
+    articles = ArticleModel.objects.filter(article_category=category,
+                                           article_sub_category=sub_category).order_by('-article_date')
+    paginator = Paginator(articles, 10)
+    page = int(page)
+    if page == 1:
+        pre_page = page
+        next_page = page + 1
+    elif page == paginator.num_pages:
+        pre_page = page - 1
+        next_page = paginator.num_pages
+    else:
+        pre_page = page - 1
+        next_page = page + 1
+    page_dict = {
+        'articles': paginator.page(1),
+        'pre_page': pre_page,
+        'next_page': next_page,
+        'page': page,
+        'pages': paginator.num_pages,
+        'category': category,
+        'sub_category': sub_category,
+    }
+    return render(request, 'focus/page.html'.format(category), page_dict)
