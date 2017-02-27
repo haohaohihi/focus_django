d342b6c234aea2b6cc272ee1df5b8668f7a527fc
haohao <haohaohihi@gmail.com>
2017-02-27 11:06:13 +0800

    complete urls

--- focus/urls.py
+++ focus/urls.py
@@ -4,4 +4,13 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url('^products/(?P<page>\d+)/$', views.products_page, name='products_page'),
+    url('^news/(?P<page>\d+)/$', views.news_page, name='news_page'),
+    url('^design/(?P<page>\d+)/$', views.design_page, name='design_page'),
+    url('^marketing/(?P<page>\d+)/$', views.marketing_page, name='marketing_page'),
+    url('^development/web/(?P<page>\d+)', views.dev_web_page, name='dev_web_page'),
+    url('^development/python/(?P<page>\d+)', views.dev_python_page, name='dev_python_page'),
+    url('^development/linux/(?P<page>\d+)', views.dev_linux_page, name='dev_linux_page'),
+    url('^development/db/(?P<page>\d+)', views.dev_db_page, name='dev_db_page'),
+    url('^development/android/(?P<page>\d+)', views.dev_android_page, name='dev_android_page'),
 ]
--- focus/views.py
+++ focus/views.py
@@ -1,10 +1,23 @@
 import logging
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.paginator import Paginator
+from .models import ArticleModel
 # Create your views here.
 
 logger = logging.getLogger("focus")
 
+
 def index(request):
 	logger.info("test logging")
-	return HttpResponse("hello")
+	products = ArticleModel.objects.filter(article_category="products")
+	paginator = Paginator(products, 10)
+	page = 1
+	page_dict = {
+		'products': paginator.page(1),
+		'pre_page': page,
+		'next_page': page + 1,
+		'page': page,
+		'pages': paginator.num_pages,
+	}
+	return render(request, 'index.html', page_dict)
