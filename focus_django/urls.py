6449d73a26cd2854b084e7565e73fcea8dc79d2d
haohao <haohaohihi@gmail.com>
2017-03-05 11:44:46 +0800

    add favicon.ico, modify index navbar: remove dropdown menu

--- focus_django/urls.py
+++ focus_django/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('focus.urls')),
+    url(r'^favicon\.ico$', RedirectView.as_view(url='/static/focus/ico/favicon.ico')),
 ]
