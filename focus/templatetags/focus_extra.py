9c998d8e4822471dac589e692bf91c6404902f95
haohao <haohaohihi@gmail.com>
2017-03-31 13:53:27 +0800

    增加了格式化月日显示和缩短标题两个模板标签

--- focus/templatetags/focus_extra.py
+++ focus/templatetags/focus_extra.py
@@ -1,10 +1,28 @@
+import logging
 import datetime
 from django import template
 
+logger = logging.getLogger("focus")
 register = template.Library()
 
 
 def strftime(var):
-    return datetime.datetime.fromtimestamp(var).strftime('%Y/%m/%d')
+    return datetime.datetime.fromtimestamp(var).strftime('%Y-%m-%d')
+
+def short_strftime(var):
+    this_year = str(datetime.datetime.now().year)
+    if datetime.datetime.fromtimestamp(var).strftime('%Y') == this_year:
+        logger.debug("success")
+        return datetime.datetime.fromtimestamp(var).strftime('%m-%d')
+    else:
+        return datetime.datetime.fromtimestamp(var).strftime('%Y-%m-%d')
+
+def format_title(var):
+    if len(var) >= 40:
+        return var[0:37] + "..."
+    else:
+        return var
 
 register.filter('strftime', strftime)
+register.filter('short_strftime', short_strftime)
+register.filter('format_title', format_title)
