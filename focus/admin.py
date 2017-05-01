cc9bc7b305db59e18aef579e51248ebc37bc4e64
Liu Zhihao <haohaohihi@gmail.com>
2017-05-01 20:03:30 +0800

    加入后台管理

--- focus/admin.py
+++ focus/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from django.db import models
+from .models import UserCollection, Article, User
 
 # Register your models here.
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'article_source', 'article_category', 'article_sub_category', 'get_date')
+    list_filter = ('article_source', 'article_category', 'article_sub_category')
+    search_fields = ('article_title',)
+    ordering = ('-article_date', 'article_title')
+
+    def get_search_results(self, request, queryset, search_term):
+        queryset, use_distinct = super(ArticleAdmin, self).get_search_results(request, queryset, search_term)
+        try:
+            search_term_as_int = int(search_term)
+            queryset |= self.model.objects.filter(age=search_term_as_int)
+        except:
+            pass
+        return queryset, use_distinct
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_filter = ('is_staff',)
+    list_display = ('username', 'date_joined', 'last_login', 'email')
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+admin.site.register(Article, ArticleAdmin)
--- focus/models.py
+++ focus/models.py
@@ -1,3 +1,4 @@
+import datetime
 from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
@@ -13,6 +14,12 @@ class Article(models.Model):
     article_sub_category = models.CharField(max_length=50, null=True, default='')
     picture_link = models.URLField(null=True, default='')
 
+    def __str__(self):
+        return self.article_title[0:20] + "..."
+
+    def get_date(self):
+        return datetime.datetime.fromtimestamp(self.article_date).strftime('%Y-%m-%d')
+
     class Meta:
         db_table = 'article'
 
@@ -23,6 +30,9 @@ class UserCollection(models.Model):
     valid = models.BooleanField(default="False")
     add_time = models.IntegerField()
     last_change_time = models.IntegerField()
+
+    def __str__(self):
+        return self.user.username
 
     class Meta:
         db_table = 'user_collection'
